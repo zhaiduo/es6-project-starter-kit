669f281d0ce9f2098a6dcdfdb8941cd59b9874a1
zhaiduo <zhaiduo@gmail.com>
2016-03-04 00:24:25 +0800

    update

--- dist/Greeter.js
+++ dist/Greeter.js
@@ -52,6 +52,7 @@
 
       var version = '0.0.1';
       var author = 'me';
+      this.PI = 3.1415926;
       this.name = name;
       this.text = text;
       this.es6Test();
@@ -75,10 +76,66 @@
         }
       }
     }, {
+      key: 'defaultValue',
+      value: function defaultValue() {
+        var x1 = undefined,
+            y1 = undefined;
+        var x2 = undefined,
+            y2 = undefined;
+        x2 = 'a';
+        var _ref = null;
+        y2 = _ref === undefined ? 'b' : _ref;
+        var _Math = Math;
+        var log = _Math.log;
+        var sin = _Math.sin;
+        var cos = _Math.cos;
+
+        console.log('[defaultValue]: y1', y1, 'y2', y2, cos(this.PI));
+      }
+    }, {
+      key: 'testReduce',
+      value: function testReduce() {
+        var v = [1, 2, 3].reduce(function (prev, curr, currIndex) {
+          return prev + curr;
+        });
+        console.log('[testReduce]: ', v);
+      }
+    }, {
+      key: 'testBrackets',
+      value: function testBrackets() {
+        var n2 = '^^';
+        var text2 = function text2(n) {
+          return 'cool[' + n + ']';
+        };
+        var tmpl = 'return ' + '`could it be ${text(n)}?`';
+
+        var func = new Function('text', 'n', tmpl);
+
+        console.log(func(text2, n2));
+
+        function codePointLength(text) {
+          var result = text.match(/(?:[\0-\uD7FF\uE000-\uFFFF]|[\uD800-\uDBFF][\uDC00-\uDFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?:[^\uD800-\uDBFF]|^)[\uDC00-\uDFFF])/g);
+          return result ? result.length : 0;
+        }
+        console.log('codePointLength', codePointLength('嘻嘻'));
+
+
+        function escapeRegExp(str) {
+          return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+        }
+
+        var str = '/path/to/resource.html?search=query';
+        console.log('escapeRegExp', escapeRegExp(str));
+      }
+    }, {
       key: 'es6Test',
       value: function es6Test() {
         this.notRealConst();
         this.realConst();
+        this.defaultValue();
+        this.testReduce();
+        this.testBrackets();
+        console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n');
       }
     }, {
       key: 'message',
--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ export default class {
   constructor(name = 'Dear Coder', text = 'hi there') {
     const version = '0.0.1'
     let author = 'me'
+    this.PI = 3.1415926
     this.name = name
     this.text = text
     this.es6Test()
@@ -40,8 +41,50 @@ export default class {
       console.log('[realConst]: ', ex)
     }
   }
+  defaultValue() {
+    let x1, y1
+    let x2, y2
+    //[x1, y1 = 'b'] = ['a', 'a']
+    //[x2, y2 = 'b'] = ['a', undefined]
+    [x2, y2 = 'b'] = ['a', null]
+    let { log, sin, cos } = Math
+    console.log('[defaultValue]: y1', y1, 'y2', y2, cos(this.PI))
+  }
+  testReduce() {
+    let v = [1, 2, 3].reduce( (prev, curr, currIndex) => prev + curr )
+    console.log('[testReduce]: ', v)
+  }
+  testBrackets() {
+    let n2 = '^^'
+    let text2 = function(n) {
+      return 'cool['+n+']'
+    }
+    let tmpl = 'return '+'`could it be ${text(n)}?`'
+    //console.log('(parseInt.prop)', Array.from('12345678').map((x) => parseInt(x, 10)))
+    let func = new Function('text', 'n', tmpl)
+    //let func = eval.call(null, '(name) => `Hello ${name}!`');
+    console.log(func(text2, n2))
+
+    function codePointLength(text) {
+      let result = text.match(/[\s\S]/gu)
+      return result ? result.length : 0
+    }
+    console.log('codePointLength', codePointLength('嘻嘻'))
+    //console.log(/^b/y.exec('aba') === /^b/.exec('aba'))
+
+    function escapeRegExp(str) {
+      return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&")
+    }
+
+    let str = '/path/to/resource.html?search=query'
+    console.log('escapeRegExp', escapeRegExp(str))
+  }
   es6Test() {
     this.notRealConst()
     this.realConst()
+    this.defaultValue()
+    this.testReduce()
+    this.testBrackets()
+    console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n')
   }
 }
